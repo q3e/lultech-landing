96cc7630b62423d0d2cb1632911a4c96e5e5705e
3mn <nfatah41@gmail.com>
2019-07-11 16:14:07 +0300

    fix case-sensitive issue with netlify

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -91,52 +91,52 @@ const Layout = ({ children }: Props) => {
 							</a>
 						</div>
 
-											<div className="flex-1">
-												<p className="uppercase text-gray-500 md:mb-6">Links</p>
-												<ul className="list-reset mb-6">
-													<li className="mt-2 inline-block mr-2 md:block md:mr-0">
-														<a href="#" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Support</a>
-													</li>
-												</ul>
-										</div>
-										<div className="flex-1">
-											<p className="uppercase text-gray-500 md:mb-6">Legal</p>
-											<ul className="list-reset mb-6">
-													<li className="mt-2 inline-block mr-2 md:block md:mr-0">
-														<a href="#" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Terms</a>
-													</li>
-													<li className="mt-2 inline-block mr-2 md:block md:mr-0">
-														<a href="#" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Privacy</a>
-													</li>
-											</ul>
-										</div>
-										{/* <div className="flex-1">
-												<p className="uppercase text-gray-500 md:mb-6">Social</p>
-												<ul className="list-reset mb-6">
-													<li className="mt-2 inline-block mr-2 md:block md:mr-0">
-														<a href="#" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Facebook</a>
-													</li>
-													<li className="mt-2 inline-block mr-2 md:block md:mr-0">
-														<a href="#" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Linkedin</a>
-													</li>
-													<li className="mt-2 inline-block mr-2 md:block md:mr-0">
-														<a href="#" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Twitter</a>
-													</li>
-												</ul>
-										</div> */}
 									<div className="flex-1">
-										<p className="uppercase text-gray-500 md:mb-6">Company</p>
+										<p className="uppercase text-gray-500 md:mb-6">Links</p>
 										<ul className="list-reset mb-6">
 											<li className="mt-2 inline-block mr-2 md:block md:mr-0">
-												<a href="/about" className="no-underline hover:underline text-gray-800 hover:text-orange-500">About Us</a>
-											</li>
-											<li className="mt-2 inline-block mr-2 md:block md:mr-0">
-												<a href="contact" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Contact</a>
+												<a href="#" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Support</a>
 											</li>
 										</ul>
-									</div>
 								</div>
+								<div className="flex-1">
+									<p className="uppercase text-gray-500 md:mb-6">Legal</p>
+									<ul className="list-reset mb-6">
+											<li className="mt-2 inline-block mr-2 md:block md:mr-0">
+												<a href="#" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Terms</a>
+											</li>
+											<li className="mt-2 inline-block mr-2 md:block md:mr-0">
+												<a href="#" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Privacy</a>
+											</li>
+									</ul>
+								</div>
+								{/* <div className="flex-1">
+										<p className="uppercase text-gray-500 md:mb-6">Social</p>
+										<ul className="list-reset mb-6">
+											<li className="mt-2 inline-block mr-2 md:block md:mr-0">
+												<a href="#" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Facebook</a>
+											</li>
+											<li className="mt-2 inline-block mr-2 md:block md:mr-0">
+												<a href="#" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Linkedin</a>
+											</li>
+											<li className="mt-2 inline-block mr-2 md:block md:mr-0">
+												<a href="#" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Twitter</a>
+											</li>
+										</ul>
+								</div> */}
+							<div className="flex-1">
+								<p className="uppercase text-gray-500 md:mb-6">Company</p>
+								<ul className="list-reset mb-6">
+									<li className="mt-2 inline-block mr-2 md:block md:mr-0">
+										<a href="/about" className="no-underline hover:underline text-gray-800 hover:text-orange-500">About Us</a>
+									</li>
+									<li className="mt-2 inline-block mr-2 md:block md:mr-0">
+										<a href="contact" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Contact</a>
+									</li>
+								</ul>
+							</div>
 						</div>
+				</div>
 			</footer>
 		</div>
 	)
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 import notFoundillustration from "../images/not-found-illustration.svg"
 
